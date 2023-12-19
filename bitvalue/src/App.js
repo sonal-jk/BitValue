ebc8738d597fc45bc1fcd8c7e96f59ee7d0c7166
Sonal <sonal1024jk@gmail.com>
2023-12-19 20:07:22 +0530

    commit and push

--- bitvalue/src/App.js
+++ bitvalue/src/App.js
@@ -1,14 +1,27 @@
 //API-key:4b5831d4-1092-4e16-ad54-794a8981ffc6
 import './App.css';
 import axios from 'axios';
+import Bcoin from './Bcoin';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [bit,changebit]=useState([])
+  const [search,setSearch]=useState('')
+
+  useEffect(()=>{
   axios.defaults.headers['Authorization']='Bearer 4b5831d4-1092-4e16-ad54-794a8981ffc6'
-  axios.get("https://api.coincap.io/v2/assets").then(a=>{console.log(a.data)}).catch(e=>{console.log(e)})
+  axios.get("https://api.coincap.io/v2/assets")
+  .then(a=>{
+    changebit(a.data)
+    console.log(a.data)}).catch(e=>{console.log(e)})},[])
+
+  //const filter=bit.filter(c=>{return c.name.toLowerCase()})
+ 
   return (
-    <div className="App">
-    
-    </div>
+    /*<>{Object.keys(bit).forEach(function(key) {
+      console.log(Object.values(bit)); 
+  })}</>*/
+  <>{Object.keys(bit).map()}</>
   );
 }
 
--- bitvalue/src/Bcoin.js
+++ bitvalue/src/Bcoin.js
@@ -1,1 +1,10 @@
 import React from "react";
+function Bcoin({rank,name,symbol,price,change,volume}){
+    return (<div><div>{rank}</div>
+    <div><h1>{name}</h1></div>
+    <div>{symbol}</div>
+    <div>{price}</div>
+    <div>{change}</div><div>{volume}</div></div>)
+
+}
+export default Bcoin;
