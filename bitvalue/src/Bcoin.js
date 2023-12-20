35ebbd7a4da5fa323f2b09aa8190e78e8ec24777
Sonal <sonal1024jk@gmail.com>
2023-12-20 21:35:09 +0530

    commit and push

--- bitvalue/src/Bcoin.js
+++ bitvalue/src/Bcoin.js
@@ -1,12 +1,13 @@
 import React from "react";
 import './App.scss'
-function Bcoin({rank,name,symbol,price,change,volume}){
-    return (<tr><td>{rank}</td>
+function Bcoin({rank,name,symbol,price,change,volume,cap}){
+    return (<tr><td>{rank}.</td>
     <td>{name}</td>
     <td>{symbol}</td>
-    <td>{price}</td>
-    <td>{change}</td>
-    <td>{volume}</td></tr>)
+    <td>${Number.parseFloat(price).toFixed(2)}</td>
+    <td>{Number.parseFloat(change).toFixed(2)} % </td>
+    <td>${Number.parseFloat(cap).toFixed(0)}</td>
+    <td>{Number.parseFloat(volume).toFixed(0)}</td></tr>)
 
 }
 export default Bcoin;
