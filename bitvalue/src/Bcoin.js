a59e24e1c28a6d3a2789f9ab207f780ce5b4d7fc
Sonal <sonal1024jk@gmail.com>
2023-12-22 00:19:35 +0530

    commit and push

--- bitvalue/src/Bcoin.js
+++ bitvalue/src/Bcoin.js
@@ -1,11 +1,16 @@
 import React from "react";
 import './App.scss'
+
+//var a=document.getElementById("Change")
 function Bcoin({rank,name,symbol,price,change,volume,cap}){
+    const style1={color:'red'}
+    const style2={color:'green'}
+    //(a.<0) ? (a.style.color='red') :(a.style.color='green');
     return (<div className="tr"><p>{rank}.</p>
     <p>{name}</p>
     <p>{symbol}</p>
     <p>${Number.parseFloat(price).toFixed(2)}</p>
-    <p>{Number.parseFloat(change).toFixed(2)} % </p>
+    {Number.parseFloat(change) <0 ?(<p id="Change" style={style1} >{Number.parseFloat(change).toFixed(2)} % </p>):(<p id="Change" style={style2} >{Number.parseFloat(change).toFixed(2)} % </p>)}
     <p>${Number.parseFloat(cap).toFixed(0)}</p>
     <p>{Number.parseFloat(volume).toFixed(0)}</p></div>)
 
